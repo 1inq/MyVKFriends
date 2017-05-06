77f7082383d50c089e9cd5ae29ffadb1d1e2cfcc
Александр Сорокин <aleksandrsorokin@ip-192-168-0-102.is74.loc>
2017-05-06 16:19:20 +0500

    commit

--- APITest/ASViewController.m
+++ APITest/ASViewController.m
@@ -7,19 +7,24 @@
 //
 
 #import "ASViewController.h"
-#import "AFNetworking.h"
+#import "ASServerManager.h"
 
 @interface ASViewController ()
 
-@property (strong, nonatomic) NSArray* friendsArray;
+@property (strong, nonatomic) NSMutableArray* friendsArray;
 
 @end
 
 @implementation ASViewController
 
+static NSInteger friendsInRequest = 5;
+
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.friendsArray = [NSMutableArray array];
+    [self getFriendsFromServer];
 }
 
 
@@ -32,7 +37,29 @@
 
 - (void) getFriendsFromServer {
     
+    NSString *userID = @"id2531409";
     
+    [[ASServerManager sharedManager] getFriendsForUserID:userID
+        offset:[self.friendsArray count]
+        count:friendsInRequest
+        onSuccess:^(NSArray *friends) {
+        
+        
+        [self.friendsArray addObjectsFromArray:friends];
+        //[self.tableView reloadData];
+        
+        NSMutableArray *newPaths = [NSMutableArray array];
+        for ( int i = (int)[self.friendsArray count] - (int)[friends count]; i < [ self.friendsArray count]; i++) {
+            [newPaths addObject:[NSIndexPath indexPathForRow: i inSection:0]];
+        }
+        
+        [self.tableView beginUpdates];
+        [self.tableView insertRowsAtIndexPaths:newPaths withRowAnimation:UITableViewRowAnimationTop];
+        [self.tableView endUpdates];
+        
+    } onFailure:^(NSError *error, NSInteger *statusCode) {
+        NSLog(@"Error = %@ , code = %d", [error localizedDescription], (int)statusCode);
+    }];
     
 }
 
